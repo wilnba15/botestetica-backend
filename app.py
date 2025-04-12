1f79c4a263d8a49a95dcd2d63febf90e6b06e3d1
wilnba15 <wsvch05@gmail.com>
2025-04-12 15:57:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,9 @@ def ver_citas():
     conn.close()
     return jsonify(citas)
 
+
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
